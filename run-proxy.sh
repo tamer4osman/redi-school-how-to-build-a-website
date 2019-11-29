8fa1631df2d744e3526910560c5ff566e30eb212
Chris Thomas <christopher.thomas@plista.com>
2019-11-29 12:05:31 +0100

    update the proxy scripts

--- run-proxy.sh
+++ run-proxy.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-docker network create redi_school
+docker network create redi_school &>/dev/null && echo "Network was created" || echo "Network already existed"
 
-docker run -d --restart always \
-        --name redi_frontend_proxy \
-        --network redi_school \
-        -p 80:80 \
-        -p 443:443 \
-        -v /var/run/docker.sock:/tmp/docker.sock:ro \
-        christhomas/nginx-proxy:alpine
+if [ -z "$(docker ps | grep redi_frontend_proxy)" ]; then
+  container_id=$(docker run -d --restart always \
+          --name redi_frontend_proxy \
+          --network redi_school \
+          -p 80:80 \
+          -p 443:443 \
+          -v /var/run/docker.sock:/tmp/docker.sock:ro \
+          christhomas/nginx-proxy:alpine)
+
+  echo "Proxy started, container id = ${container_id}"
+else
+  echo "Proxy was already running"
+fi
--- stop-proxy.sh
+++ stop-proxy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker network rm redi_school
-docker kill redi_frontend_proxy
+docker kill redi_frontend_proxy 2>/dev/null && (docker container rm redi_frontend_proxy &>/dev/null && echo "Proxy was stopped") || echo "Proxy was not running (already stopped?)"
+docker network rm redi_school 2>/dev/null && echo "Network was deleted" || echo "Network was not found (already deleted?)"
