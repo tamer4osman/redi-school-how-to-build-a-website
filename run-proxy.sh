34f66300574bab62872a4bcfae2e256f45c63057
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-11-29 12:07:56 +0100

    update the proxy scripts

--- run-proxy.sh
+++ run-proxy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Don't be afraid of how complicated this looks, it's far simpler in reality than it seems
+# I just added some strings to be echo'd to the terminal when things succeed or fail
+# But the raw commands themselves are simple enough
+
 docker network create redi_school &>/dev/null && echo "Network was created" || echo "Network already existed"
 
 if [ -z "$(docker ps | grep redi_frontend_proxy)" ]; then
--- stop-proxy.sh
+++ stop-proxy.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# Don't be afraid of how complicated this looks, it's far simpler in reality than it seems
+# I just added some strings to be echo'd to the terminal when things succeed or fail
+# But the raw commands themselves are simple enough
+
 docker kill redi_frontend_proxy 2>/dev/null && (docker container rm redi_frontend_proxy &>/dev/null && echo "Proxy was stopped") || echo "Proxy was not running (already stopped?)"
 docker network rm redi_school 2>/dev/null && echo "Network was deleted" || echo "Network was not found (already deleted?)"
